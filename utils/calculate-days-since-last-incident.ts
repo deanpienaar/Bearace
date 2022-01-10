ad4b12c4dc97fab580c729c5d0990b5c6b02e873
Dean Pienaar <dean.pienaar@outlook.com>
2022-01-10 17:43:46 +0100

    Calc Reset from 8am

--- utils/calculate-days-since-last-incident.ts
+++ utils/calculate-days-since-last-incident.ts
@@ -2,7 +2,7 @@ import {DateTime} from 'luxon';
 
 
 export function calculateDaysSinceLastIncident (lastIncident: Date) {
-  const date = DateTime.fromJSDate(lastIncident);
+  const date = DateTime.fromJSDate(lastIncident).set({hour: 8}).startOf('hour');
   const now = DateTime.now();
 
   return Math.floor(now.diff(date).as('days'));
